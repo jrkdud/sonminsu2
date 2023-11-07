71854064e501646c22f22235aa7ff7e2d4706d62
jrkdud <newgy28@naver.com>
2023-11-08 03:10:35 +0900

    파이어베이스로 로그인, 회원가입, 팔로우, 사람검색 구현, 디테일 수정 필요

---
+++ app/src/main/java/com/example/sonminsu/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.sonminsu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.example.sonminsu.Fragment.HomeFragment;
+import com.example.sonminsu.Fragment.NotificationFragment;
+import com.example.sonminsu.Fragment.PostFragment;
+import com.example.sonminsu.Fragment.ProfileFragment;
+import com.example.sonminsu.Fragment.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    HomeFragment homeFragment;
+    SearchFragment searchFragment;
+    PostFragment postFragment;
+    NotificationFragment notificationFragment;
+    ProfileFragment profileFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+        searchFragment = new SearchFragment();
+        postFragment = new PostFragment();
+        notificationFragment = new NotificationFragment();
+        profileFragment = new ProfileFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
+
+        NavigationBarView navigationBarView = findViewById(R.id.bottom_navigation);
+        navigationBarView.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_home) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
+                return true;
+            } else if (itemId == R.id.nav_search) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, searchFragment).commit();
+                return true;
+            } else if (itemId == R.id.nav_add) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, postFragment).commit();
+                return true;
+            } else if (itemId == R.id.nav_notification) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, notificationFragment).commit();
+                return true;
+            } else if (itemId == R.id.nav_profile) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, profileFragment).commit();
+                return true;
+            }
+
+            return false;
+        });
+    }
+}
---
+++ app/src/main/java/com/example/sonminsu/RegisterActivity.java
@@ -0,0 +1,102 @@
+package com.example.sonminsu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText username, fullname, email, password;
+    Button register;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
+    ProgressDialog pd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.username);
+        fullname = findViewById(R.id.fullname);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+
+        auth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str_username = username.getText().toString();
+                String str_fullname = fullname.getText().toString();
+                String str_email = email.getText().toString();
+                String str_password = password.getText().toString();
+
+                if(TextUtils.isEmpty(str_username) || TextUtils.isEmpty(str_fullname) || TextUtils.isEmpty(str_email) || TextUtils.isEmpty(str_password)) {
+                    Toast.makeText(RegisterActivity.this, "모든 입력란을 채워주세요", Toast.LENGTH_SHORT).show();
+                } else if (str_password.length() < 6) {
+                    Toast.makeText(RegisterActivity.this, "비밀번호는 6자이상 입력해주세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    register(str_username, str_fullname, str_email, str_password);
+                }
+            }
+        });
+    }
+    private void register(String username, String fullname, String email, String password){
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser firebaseUser = auth.getCurrentUser();
+                    String userid = firebaseUser.getUid();
+
+                    reference = FirebaseDatabase.getInstance().getReference().child("Users").child(userid);
+
+                    HashMap<String, Object> hashMap = new HashMap<>();
+                    hashMap.put("id", userid);
+                    hashMap.put("username", username.toLowerCase());
+                    hashMap.put("fullname", fullname);
+                    hashMap.put("bio", "");
+                    hashMap.put("imageurl", "https://firebasestorage.googleapis.com/v0/b/smsfirebase-cbc65.appspot.com/o/person.png?alt=media&token=9dcd124e-fe98-4cd5-9af3-17ee6dfcf603");
+
+                    reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                pd.dismiss();
+                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+
+                } else {
+                    pd.dismiss();
+                    Toast.makeText(RegisterActivity.this, "입력하신 이메일과 비밀번호는 가입할 수 없습니다.", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+}
